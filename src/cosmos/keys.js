774814dc684f885207f8e4806d88ecff151f9686
sim <sim@terra.money>
2019-09-11 12:47:18 +0900

    refactor(Packages): Remove @lunie/cosmos-keys

--- src/cosmos/keys.js
+++ src/cosmos/keys.js
@@ -1,4 +1,5 @@
 import * as bech32 from 'bech32'
+import * as secp256k1 from 'secp256k1'
 import * as CryptoJS from 'crypto-js'
 
 export const getTerraAddress = publicKey => {
@@ -10,4 +11,16 @@ export const getTerraAddress = publicKey => {
 const bech32ify = (address, prefix) => {
   const words = bech32.toWords(Buffer.from(address, 'hex'))
   return bech32.encode(prefix, words)
+}
+
+export const signWithPrivateKey = (signMessage, privateKey) => {
+  const signMessageString =
+    typeof signMessage === 'string' ? signMessage : JSON.stringify(signMessage)
+  const signHash = Buffer.from(
+    CryptoJS.SHA256(signMessageString).toString(),
+    `hex`
+  )
+  const { signature } = secp256k1.sign(signHash, privateKey)
+
+  return signature
 }
