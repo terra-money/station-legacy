0a9b5541a9cb9399990f1ecc70293f026f675ce9
sim <sim@terra.money>
2019-10-17 15:43:41 +0900

    feature(ValidatorsList): Sort columns

--- src/pages/staking/ValidatorsList.tsx
+++ src/pages/staking/ValidatorsList.tsx
@@ -1,20 +1,33 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useState } from 'react'
+import { path, equals } from 'ramda'
 import { gt, plus, sum } from '../../api/math'
 import FlexTable from '../../components/FlexTable'
+import Icon from '../../components/Icon'
 import renderItem from './ValidatorItem'
 
 type Attr = { align?: 'center' | 'right'; style: CSSProperties }
-const Columns: [string, Attr][] = [
+type Sorter = { prop: string[]; isString?: boolean }
+const DefaultSorter: Sorter = { prop: ['votingPower', 'weight'] }
+const Columns: [string, Attr, Sorter?][] = [
   ['Rank', { style: { width: 60 } }],
-  ['Moniker', { style: { width: 240 } }],
-  ['Voting power', { align: 'right', style: { width: 120 } }],
+  [
+    'Moniker',
+    { style: { width: 240 } },
+    { prop: ['description', 'moniker'], isString: true }
+  ],
+  ['Voting power', { align: 'right', style: { width: 120 } }, DefaultSorter],
   [
     'Validator commission',
-    { align: 'right', style: { width: 100, textAlign: 'right' } }
+    { align: 'right', style: { width: 100, textAlign: 'right' } },
+    { prop: ['commissionInfo', 'rate'] }
   ],
-  ['Uptime', { align: 'right', style: { width: 80 } }],
+  ['Uptime', { align: 'right', style: { width: 80 } }, { prop: ['upTime'] }],
   ['', { style: { width: 20 } }],
-  ['My delegation', { align: 'right', style: { width: 200 } }]
+  [
+    'My delegation',
+    { align: 'right', style: { width: 200 } },
+    { prop: ['myDelegation'] }
+  ]
 ]
 
 const ValidatorsList = (staking: Staking) => {
@@ -24,15 +37,45 @@ const ValidatorsList = (staking: Staking) => {
       ? sum(undelegations.map(u => u.amount))
       : '0'
 
+  /* sort */
+  const [asc, setAsc] = useState<boolean>(false)
+  const [sorter, setSorter] = useState<Sorter>(DefaultSorter)
+  const { prop, isString } = sorter
+
   return (
     <FlexTable
-      head={Columns.map(([h]) => h)}
+      head={Columns.map(([h, , s]) => {
+        const isPropSelected = equals(sorter, s)
+        const icon = isPropSelected && asc ? 'arrow_drop_up' : 'arrow_drop_down'
+
+        const handleClick = () => {
+          !isPropSelected && s && setSorter(s)
+          setAsc(a => (isPropSelected ? !a : false))
+        }
+
+        const button = (
+          <button onClick={handleClick} className="flex">
+            {h}
+            <Icon name={icon} style={{ opacity: isPropSelected ? 1 : 0.3 }} />
+          </button>
+        )
+
+        return !s ? h : button
+      })}
       body={validators
         .filter(v => {
           const delegated = v.myDelegation && gt(v.myDelegation, 0)
           const hidden = v.status === 'jailed' && !delegated
           return !hidden
         })
+        .sort((validatorA, validatorB) => {
+          const a: string = path(prop, validatorA) || '0'
+          const b: string = path(prop, validatorB) || '0'
+          const compareString = asc ? (a > b ? 1 : -1) : a > b ? -1 : 1
+          const compareNumber = asc ? (gt(a, b) ? 1 : -1) : gt(a, b) ? -1 : 1
+          return a === b ? 0 : isString ? compareString : compareNumber
+        })
+
         .map((validator, index) =>
           renderItem({
             validator,
