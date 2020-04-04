8ab8c10a5b3adb02e1bd0537d55c465fe22ed588
sim <sim@terra.money>
2020-04-04 23:21:11 +0900

    Add validator email addresses

--- src/validators.ts
+++ src/validators.ts
@@ -2,6 +2,29 @@ import { Dictionary } from 'ramda'
 
 // prettier-ignore
 const list: Dictionary<string> = {
+  terravaloper123gn6j23lmexu0qx5qhmgxgunmjcqsx8gmsyse: 'go@staking.fund',
+  terravaloper13kcwnlafvu4xvy2jr3vhdte9aq9tadwds3lx2d: 'gautier@castlenode.com',
+  terravaloper144l7c3uph5a7h62xd8u5et3rqvj3dqtvvka2fu: 'vladimir.m@p2p.org',
+  terravaloper15cupwhpnxhgylxa8n4ufyvux05xu864jcv0tsw: 'matt@figment.network',
+  terravaloper18825kzx0zmdntpucvd2gjezau57vdyua6frdnh: 'support@ubik.capital',
+  terravaloper1c6gve6zhye5690563wxmvns7mugz6plu4aj7d3: 'ninorjmansor@gmail.com',
+  terravaloper1cac2mcf2eszn9ln3fx4heym6kd363zqfelxrmr: 'iryoung@fairsquarelab.com',
+  terravaloper1d0vfj9zvxfgcm4yt4ze4u35mvhj57eg2ku2ekv: 'contact@easy2stake.com',
+  terravaloper1fjuvyccn8hfmn5r7wc2t3kwqy09zzp6tyjcf50: 'contact@de-light.io',
+  terravaloper1gh7wpfpsjrqnash5uc84z4njt95y9g5nh3uqzx: 'genesislab@protonmail.com',
+  terravaloper1hz754zdldnrrhp3qpfan3l2dxkcv5cgkuzqq9v: 'nmiller@consensusnetworks.com',
+  terravaloper1jkqr2vfg4krfd4zwmsf7elfj07cjuzss30ux8g: 'kwun@forbole.com',
+  terravaloper1jyjg55hzsh0f4xymy0kuuan30pp4q75ruqmvyt: 'calin.chitu@gmail.com',
+  terravaloper1kgddca7qj96z0qcxr2c45z73cfl0c75paknc5e: 'support@chainlayer.io',
+  terravaloper1khfcg09plqw84jxy5e7fj6ag4s2r9wqsgm7k94: 'claudiu@01node.com',
+  terravaloper1kprce6kc08a6l03gzzh99hfpazfjeczfpzkkau: 'hendrik@certus.one',
+  terravaloper1lda78gzrjx0rsadtdk0zn4v7awtz6m9lrd5ez9: 'roman@polychain.capital',
+  terravaloper1pc0gs3n6803x7jqe9m7etegmyx29xw38aaf3u7: 'hello@staker.space',
+  terravaloper1sym8gyehrdsm03vdc44rg9sflg8zeuqwfzavhx: 'buneageorge@yahoo.com',
+  terravaloper1uhjx34pfsxk9xh34yn8p2w4469uqdz067rqu5g: 'luigi@umbrellavalidator.com',
+  terravaloper1v5hrqlv8dqgzvy0pwzqzg0gxy899rm4kdur03x: 'aurel@ethereum.ro',
+  terravaloper1vqnhgc6d0jyggtytzqrnsc40r4zez6tx99382w: 'developers@kysenpool.io',
+  terravaloper1yad8pjqp93gvwkxa2aa5mh4vctzfs37ekjxr4s: 'heechl@naver.com',
 }
 
 export default list
