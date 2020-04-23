6b3412db70cdf6f5d8b7603a1f62d389e8126f9f
sim <sim@terra.money>
2020-04-23 18:36:14 +0900

    update(Staking): Verify validators who have submitted their email addresses

--- src/validators.ts
+++ src/validators.ts
@@ -2,14 +2,21 @@ import { Dictionary } from 'ramda'
 
 // prettier-ignore
 const list: Dictionary<string> = {
+  terravaloper15urq2dtp9qce4fyc85m6upwm9xul30496sgk37: 'hello@chorus.one',
+  terravaloper15zcjduavxc5mkp8qcqs9eyhwlqwdlrzy6jln3m: 'contact@bharvest.io',
   terravaloper16tc3c9u6yj5uuhru32pvs0pahfwraurpypz7vj: 'office@block42.tech',
   terravaloper1audgfvmgt0js54p3s8kj3r40uwej6vy2tv6rrw: 'info@mitera.net',
   terravaloper1aw0znxtlq0wrayyz7wppz3qnw94hfrmnnrcxja: 'support@rockx.com',
   terravaloper1c9ye54e3pzwm3e0zpdlel6pnavrj9qqvq89r3r: 'Earn@StakeWith.Us',
   terravaloper1fjuvyccn8hfmn5r7wc2t3kwqy09zzp6tyjcf50: 'contact@delightlabs.io',
+  terravaloper1h6rf7y2ar5vz64q8rchz5443s3tqnswrpf4846: 'staked@staked.us',
+  terravaloper1hz754zdldnrrhp3qpfan3l2dxkcv5cgkuzqq9v: 'info@consensusnetworks.com',
   terravaloper1jyjg55hzsh0f4xymy0kuuan30pp4q75ruqmvyt: 'calin.chitu@gmail.com',
+  terravaloper1kgddca7qj96z0qcxr2c45z73cfl0c75paknc5e: 'hello@chainlayer.io',
+  terravaloper1khfcg09plqw84jxy5e7fj6ag4s2r9wqsgm7k94: 'hello@01node.com',
   terravaloper1nwrksgv2vuadma8ygs8rhwffu2ygk4j24w2mku: 'hello@posbakerz.com',
   terravaloper1pc0gs3n6803x7jqe9m7etegmyx29xw38aaf3u7: 'hello@staker.space',
+  terravaloper1sym8gyehrdsm03vdc44rg9sflg8zeuqwfzavhx: 'support@syncnode.ro',
   terravaloper1tusfpgvjrplqg2fm7wacy4slzjmnzswcfufuvp: 'contact@nodeateam.com',
   terravaloper1uhjx34pfsxk9xh34yn8p2w4469uqdz067rqu5g: 'hello@umbrellavalidator.com',
   terravaloper1vse7mfah2hmfaruum40hwnskkca3ygmmff86xr: 'jooddang@gmail.com',
