a42e69aed850a24f16fe91c81a7279df94d92ece
sim <sim@terra.money>
2020-06-01 14:47:05 +0900

    fix(Tabs): Prevent side effect when use URLSearchParams

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -12,6 +12,7 @@ export default (): [
 
   // const queries =
   const getNextSearch = (entries: [string, string][]) => {
+    const sp = new URLSearchParams(search) // Prevent side effect
     entries.forEach(([key, value]) =>
       value ? sp.set(key, value) : sp.delete(key)
     )
