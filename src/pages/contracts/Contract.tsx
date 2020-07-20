dfc73b69e57b569b4d62d144090477549493df58
sim <sim@terra.money>
2020-07-20 18:05:51 +0900

    feature(contracts): Show date and name of code and contracts

--- src/pages/contracts/Contract.tsx
+++ src/pages/contracts/Contract.tsx
@@ -1,20 +1,40 @@
 import React from 'react'
+import c from 'classnames'
 import { ContractUI } from '@terra-money/use-station'
 import { useApp } from '../../hooks'
 import ButtonWithAuth from '../../components/ButtonWithAuth'
 import ActionBar from '../../components/ActionBar'
 import Card from '../../components/Card'
 import Flex from '../../components/Flex'
+import Icon from '../../components/Icon'
 import Interact from '../../post/Interact'
 import Query from '../../post/Query'
+import s from '../txs/Tx.module.scss'
 
-const Contract = ({ address, interact, query }: ContractUI) => {
+const Contract = ({ address, date, ...props }: ContractUI) => {
+  const { contract, code, interact, query } = props
   const { modal } = useApp()
 
+  const title = (
+    <>
+      <span className={s.address}>{address}</span>
+      <section className={s.date}>
+        <Icon name="date_range" />
+        {date}
+      </section>
+    </>
+  )
+
   return (
-    <Card small>
+    <Card title={title} {...classNames} bordered>
       <Flex className="space-between">
-        {address}
+        <dl className={c('dl-wrap', s.dl)}>
+          <dt>{code.label}</dt>
+          <dd>{code.value}</dd>
+          <dt>{contract.label}</dt>
+          <dd>{contract.value}</dd>
+        </dl>
+
         <ActionBar
           list={[
             <ButtonWithAuth
@@ -37,3 +57,9 @@ const Contract = ({ address, interact, query }: ContractUI) => {
 }
 
 export default Contract
+
+/* styles */
+const classNames = {
+  headerClassName: s.header,
+  bodyClassName: s.body,
+}
