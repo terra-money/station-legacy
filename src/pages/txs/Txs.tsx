13e1c217142254464fe46f8cd099c03666095cae
sim <sim@terra.money>
2021-03-24 13:08:23 +0900

    fix: Reload on change tab on tx history

--- src/pages/txs/Txs.tsx
+++ src/pages/txs/Txs.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { TxType, TxsUI, User } from '../../use-station/src'
 import { useMenu, useTxs, useTxTypes } from '../../use-station/src'
-import usePageTabs from '../../hooks/useTabs'
+import useTabs from '../../hooks/useTabs'
 import WithAuth from '../../auth/WithAuth'
 import Page from '../../components/Page'
 import Info from '../../components/Info'
@@ -12,17 +12,21 @@ import ErrorBoundary from '../../components/ErrorBoundary'
 import ErrorComponent from '../../components/ErrorComponent'
 import Tx from './Tx'
 
-const List = ({ user }: { user: User }) => {
-  const tabs = useTxTypes()
-  const { currentTab, renderTabs } = usePageTabs('tag', tabs)
+interface Props {
+  user: User
+  currentTab: string
+  renderTabs: () => ReactNode
+}
+
+const List = ({ user, currentTab, renderTabs }: Props) => {
   const params = { type: currentTab as TxType }
-  const { error, ui } = useTxs(user, params)
+  const { loading, error, ui } = useTxs(user, params)
 
   const render = ({ card, list, more }: TxsUI) => {
     const empty = card && <Info {...card} icon="info_outline" />
 
     return (
-      <More isEmpty={!list?.length} empty={empty} more={more}>
+      <More isEmpty={!loading && !list?.length} empty={empty} more={more}>
         {list?.map((tx, index) => (
           <ErrorBoundary fallback={<ErrorComponent />} key={index}>
             <Tx {...tx} />
@@ -41,10 +45,14 @@ const List = ({ user }: { user: User }) => {
 
 const Txs = () => {
   const { History: title } = useMenu()
+  const txTypes = useTxTypes()
+  const tabs = useTabs('tag', txTypes)
 
   return (
     <Page title={title}>
-      <WithAuth card>{(user) => <List user={user} />}</WithAuth>
+      <WithAuth card>
+        {(user) => <List user={user} {...tabs} key={tabs.currentTab} />}
+      </WithAuth>
     </Page>
   )
 }
