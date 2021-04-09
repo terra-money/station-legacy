f55a28de9812f72bb36f9072cb86e54f4e64efc7
Paul Kim <paul@terra.money>
2021-04-09 21:21:07 +0900

    Remove websocket (#156)
    * remove websocket
    * feat(use-station): use lightest endpoint for getting height

--- src/layouts/Height.tsx
+++ src/layouts/Height.tsx
@@ -1,44 +1,44 @@
-import React, { useEffect } from 'react'
-import { toast } from 'react-toastify'
-import semver from 'semver'
-import { useSocket, Block } from '../use-station/src'
+import React from 'react'
+//import { toast } from 'react-toastify'
+// import semver from 'semver'
+import { useHeight, HeightData } from '../use-station/src'
 import Flex from '../components/Flex'
 import ExtLink from '../components/ExtLink'
 import Icon from '../components/Icon'
-import Update from './Update'
+// import Update from './Update'
 import s from './Height.module.scss'
 
+/* helper */
+// const parseVersion = (s: string): VersionWeb | undefined => {
+//   try {
+//     return JSON.parse(s)
+//   } catch (error) {
+//     return undefined
+//   }
+// }
+
 const Height = () => {
-  const { block, status } = useSocket()
+  const height = useHeight()
 
   /* update toast */
-  useEffect(() => {
-    const parsed = status ? parseVersion(status) : undefined
-    const currentVersion = process.env.REACT_APP_VERSION
-    const shouldUpdate =
-      parsed && currentVersion && !semver.gte(currentVersion, parsed.version)
+  // useEffect(() => {
+  //   const parsed = status ? parseVersion(status) : undefined
+  //   const currentVersion = process.env.REACT_APP_VERSION
+  //   const shouldUpdate =
+  //     parsed && currentVersion && !semver.gte(currentVersion, parsed.version)
 
-    shouldUpdate && toast(<Update {...parsed!} />, { toastId: 'App Update' })
-    // eslint-disable-next-line
-  }, [status])
+  //   shouldUpdate && toast(<Update {...parsed!} />, { toastId: 'App Update' })
+  //   // eslint-disable-next-line
+  // }, [status])
 
-  const render = ({ formatted, link }: Block) => (
+  const render = ({ formatted, link }: HeightData) => (
     <>
       <ExtLink href={link}>{formatted}</ExtLink>
       <Icon name="open_in_new" size={12} />
     </>
   )
 
-  return <Flex className={s.height}>{block && render(block)}</Flex>
+  return <Flex className={s.height}>{height && render(height)}</Flex>
 }
 
 export default Height
-
-/* helper */
-const parseVersion = (s: string): VersionWeb | undefined => {
-  try {
-    return JSON.parse(s)
-  } catch (error) {
-    return undefined
-  }
-}
