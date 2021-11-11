3c070fb9c10f89ed480cb238f28a1e5b269af372
Seo Yeon, Lee <i@ssen.name>
2021-11-11 19:52:01 +0900

    feat: Add window.terraWallets rule for support multiple extensions (#214)
    * feat: add window.terraWallets rule for support multiple extensions
    * fix: remove <script async> attribute

--- extension/inpage.js
+++ extension/inpage.js
@@ -1,1 +1,19 @@
-window.isTerraExtensionAvailable = true;
+window.isTerraExtensionAvailable = true
+
+// ---------------------------------------------
+// for multiple extension support
+// ---------------------------------------------
+const TERRA_STATION_INFO = {
+  name: 'Terra Station',
+  identifier: 'station',
+  icon: 'https://assets.terra.money/icon/station-extension/icon.png',
+}
+
+if (
+  typeof window.terraWallets !== 'undefined' &&
+  Array.isArray(window.terraWallets)
+) {
+  window.terraWallets.push(TERRA_STATION_INFO)
+} else {
+  window.terraWallets = [TERRA_STATION_INFO]
+}
