3194296136faec7b561ad6f10379bae6b8f031bb
James0126 <james@terra.money>
2021-11-15 19:16:09 +0900

    Remove force typing of denom trace

--- src/data/lcd/ibc.ts
+++ src/data/lcd/ibc.ts
@@ -1,5 +1,4 @@
 import { useQuery } from 'react-query'
-import { DenomTrace } from '@terra-money/terra.js/dist/core/ibc-transfer/DenomTrace'
 import { is } from '../../utils'
 import useLCD from '../../api/useLCD'
 
@@ -10,9 +9,7 @@ export const useGetQueryDenomTrace = () => {
     queryFn: async () => {
       if (!is.ibcDenom(denom)) return
       const hash = denom.replace('ibc/', '')
-      const dt = await lcd.ibcTransfer.denomTrace(hash)
-      /* TODO: Remove force typing on terra.js fixed */
-      const { denom_trace } = dt as unknown as { denom_trace: DenomTrace }
+      const denom_trace = await lcd.ibcTransfer.denomTrace(hash)
       return denom_trace
     },
   })
