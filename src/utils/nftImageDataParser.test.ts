f9099ad6e16f172973efcb0183c121eb3cc75923
wrjanan <janan1990@gmail.com>
2021-12-09 12:07:49 +0800

    linting fix

--- src/utils/nftImageDataParser.test.ts
+++ src/utils/nftImageDataParser.test.ts
@@ -6,24 +6,18 @@ describe('nftImageDataParser', () => {
     const lunaPunkContract = 'terra1qfy2nfr0zh70jyr3h4ns9rzqx4fl8rxpf09ytv'
     const imageData = '<svg></svg>'
     it('should return undefined if empty values returned', () => {
-      const parsedImage = runCustomImageDataParsers()
-      console.log(parsedImage)
-      expect(parsedImage).toBeUndefined()
+      expect(runCustomImageDataParsers()).toBeUndefined()
     })
     it('should return undefined if image data has value but empty contract', () => {
-      const parsedImage = runCustomImageDataParsers(imageData)
-      console.log(parsedImage)
-      expect(parsedImage).toBeUndefined()
+      expect(runCustomImageDataParsers(imageData)).toBeUndefined()
     })
     it('should return undefined if image data has value but not part of custom parser contract', () => {
-      const parsedImage = runCustomImageDataParsers(imageData, mockContract)
-      console.log(parsedImage)
-      expect(parsedImage).toBeUndefined()
+      expect(runCustomImageDataParsers(imageData, mockContract)).toBeUndefined()
     })
     it('should return run custom parser if parsed LunaPunk contract', () => {
-      const parsedImage = runCustomImageDataParsers(imageData, lunaPunkContract)
-      console.log(parsedImage)
-      expect(parsedImage).not.toBeUndefined()
+      expect(
+        runCustomImageDataParsers(imageData, lunaPunkContract)
+      ).not.toBeUndefined()
     })
   })
 })
