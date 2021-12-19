af9fc5738a3f4cda82d4964f9687b945702eb52e
sim <sim@terra.money>
2021-12-20 08:41:16 +0900

    Hardcode gas price multiplied by 5.92

--- src/post/hooks/txHelpers.ts
+++ src/post/hooks/txHelpers.ts
@@ -2,12 +2,36 @@ import { Dictionary } from 'ramda'
 import { ParsedRaw, ParsedLog, PostError } from '../../types'
 import { times, div, ceil, floor } from '../../utils/math'
 import { decimal } from '../../utils/format'
-import useFCD from '../../api/useFCD'
+// import useFCD from '../../api/useFCD'
 
 export const config = { headers: { 'Content-Type': 'application/json' } }
+const data: Record<string, string> = {
+  uluna: '0.01133',
+  uusd: '0.888945',
+  uaud: '1.125998',
+  ucad: '1.125998',
+  uchf: '0.829682',
+  ucny: '5.807779',
+  udkk: '5.333674',
+  ueur: '0.740788',
+  ugbp: '0.651893',
+  uhkd: '6.933777',
+  uidr: '12919.345720',
+  uinr: '64.478202',
+  ujpy: '97.013619',
+  ukrw: '1006.108863',
+  umnt: '2539.842621',
+  unok: '7.407881',
+  uphp: '45.039920',
+  usdr: '0.621894',
+  usek: '7.407881',
+  usgd: '1.185261',
+  uthb: '27.379530',
+}
+
 export const useCalcFee = () => {
-  const url = '/v1/txs/gas_prices'
-  const { data } = useFCD<Dictionary<string>>({ url })
+  // const url = '/v1/txs/gas_prices'
+  // const { data } = useFCD<Dictionary<string>>({ url })
 
   const getGasPrice = (denom: string) => data?.[denom] ?? '0'
 
